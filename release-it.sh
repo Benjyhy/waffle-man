c7efec728563d0f93577cff060f5a0150700a1a9
RAMET Benjamin <ramet.benj@gmail.com>
2023-05-28 16:01:38 +0200

    Update release bash script

--- release-it.sh
+++ release-it.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 AMQP_RELEASE_NAME="amqp-$1"
-$ES_RELEASE_NAME="elasticsearch-$1"
+ES_RELEASE_NAME="elasticsearch-$1"
 
 kubectl create -f https://download.elastic.co/downloads/eck/2.8.0/crds.yaml
 kubectl apply -f https://download.elastic.co/downloads/eck/2.8.0/operator.yaml
