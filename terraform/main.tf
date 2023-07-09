82c3af93df145ab8082d01ee25968fd31282aec0
Paul SANTA-MARIA <paul.santa-maria@advens.fr>
2023-07-09 16:44:47 +0200

    optinal ssl

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,6 +27,7 @@ resource "ovh_cloud_project_kube_nodepool" "node_pool" {
   kube_id       = ovh_cloud_project_kube.my_kube_cluster.id
   name          = "my-pool" 
   flavor_name   = "b2-7"
+  autoscale     = true
   desired_nodes = 3
   max_nodes     = 3
   min_nodes     = 3
